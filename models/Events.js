65b792777f0c7d7801bce20e99e97c67a2eeb4c8
Sillasrocha98 <93406918+SILLASrocha97@users.noreply.github.com>
2023-09-14 22:26:44 -0300

    Add files via upload

---
+++ models/Events.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataType) => {
+    const Evento = sequelize.define('Events', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: DataType.STRING,
+        tema: DataType.STRING,
+        descricao: DataType.STRING,
+        data_inicio: DataType.DATE,
+        data_fim: DataType.DATE,
+        hora_inicio: DataType.STRING,
+        hora_fim: DataType.STRING,
+        preco: DataType.STRING,
+        inicio_vendas: DataType.DATE,
+        link_imagem: DataType.STRING,
+        link_video: DataType.STRING
+    },{
+        tableName: 'events',
+        timeStamps: false
+    })
+
+    return Evento
+}
